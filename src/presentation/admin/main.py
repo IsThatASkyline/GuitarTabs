1f85bcce790aabafce4b68bf1a15e99a017c21c7
artklk12 <artem-kulik@list.ru>
2023-08-31 20:51:16 +0300

    add template for chords

--- src/presentation/admin/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from sqladmin import Admin
-from sqlalchemy.ext.asyncio import AsyncEngine
-from src.infrastructure.admin import setup_admin_models
-
-
-def create_admin_instance(app: FastAPI, engine: AsyncEngine):
-    admin = Admin(app, engine)
-    setup_admin_models(admin)
